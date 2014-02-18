eb702b7e00b5dc3329a4be303e0db164d25044b8
lmon <luke@lucasmonaco.com>
2014-02-18 17:32:01 -0500

    Finished static changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,12 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Slideshow App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('My Slideshow App')
     end
 
-	it "should have the right title" do
+	it "should have the right title 'Slideshow App | Home'" do
 	  visit '/static_pages/home'
 	  expect(page).to have_title("Slideshow App | Home")
 	end
@@ -21,13 +21,31 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help Page')
     end
+    
+     it "should have the title 'Slideshow App | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Slideshow App | Help")
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Page'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Page')
+      # page specific content
+      #expect(page).to have_content('About Page')
+
+      # tag-specific content
+      expect(page).should have_selector("h1") do |content|
+    	expect(content).to have_content("About Page Me!") 
+  	  end
+  
     end
-  end
+          
+  	it "should have the right title 'Slideshow App | About'" do
+	  visit '/static_pages/about'
+	  expect(page).to have_title("Slideshow App | About")
+	end
+
+end
 
 end
