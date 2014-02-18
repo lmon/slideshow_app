80468463e72a65feb83b2e9c0e4574a3cbf3530f
lmon <luke@lucasmonaco.com>
2014-02-18 18:08:35 -0500

    added an empty form page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,19 +33,32 @@ describe "Static pages" do
       visit '/static_pages/about'
       # page specific content
       #expect(page).to have_content('About Page')
-
       # tag-specific content
       expect(page).should have_selector("h1") do |content|
     	expect(content).to have_content("About Page Me!") 
   	  end
-  
-    end
-          
+    end        
   	it "should have the right title 'Slideshow App | About'" do
 	  visit '/static_pages/about'
 	  expect(page).to have_title("Slideshow App | About")
 	end
+   end
+   describe "Contact page" do
+    it "should have the content 'Contact Page'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Page')
+    end
 
-end
+     it "should have the title 'Slideshow App | Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Slideshow App | Contact")
+    end
 
+    it "should have the content 'Simple static useless'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Simple static useless')
+    end
+    
+  end
+  
 end
