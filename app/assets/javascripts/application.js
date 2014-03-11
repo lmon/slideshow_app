64456ba77628156f8fe891242655788c15767b86
lmon <luke@lucasmonaco.com>
2014-03-11 19:20:17 -0400

    added drag n drop for Assets In GalleryEdit UI. started ordering for display on Gallery Edit. Added asset_order method & route for Galleries in controller.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,39 +18,82 @@
 //= require turbolinks
 //= require_tree .
 
- var AUTH_TOKEN = (AUTH_TOKEN != null ? AUTH_TOKEN : "987987987b98798798bIU&kjhhgjhgjgj7657657");
  //var gid = (gid != null ? gid : 304);
+if(!slideshow){ var slideshow = {}; }
 
 $(function() {
+
+slideshow.loader = function(mode){
+    console.log('inside load function')
+    if(mode == false){
+      //show
+      $('#dvLoading').fadeIn();
+
+    }else{
+      //hide
+      $('#dvLoading').fadeOut(1000);
+    }
+}
+
   $('#sliderArea').ramblingSlider();
+
+    $( "#sortable-toadd" ).sortable({
+       cursor: 'crosshair',
+       opacity: 0.4,
+       placeholder: "ui-state-highlight",
+       handle: '.handle',
+  });
+
+  $( "#sortable, #sortable-toadd" ).sortable({
+      connectWith: ".connectedSortable"
+    });
+
+
+
 });
 
   $(function() {
     $( "#sortable" ).sortable({
        cursor: 'crosshair',
        opacity: 0.4,
-      placeholder: "ui-state-highlight",
+       placeholder: "ui-state-highlight",
        handle: '.handle',
-	  axis: 'y',
-    update: function(){
+      update: function(){
         if(!$( "#sortable" ).attr( "gid" )){alert('there was an error');return;}
+        
+        AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
 
-         $.post('/galleries/'+$( "#sortable" ).attr( "gid" )+'sort', 
-        '_method=put&authenticity_token='+AUTH_TOKEN+'&'+$(this).sortable('serialize'));
-    }
+        if( AUTH_TOKEN == null ){ alert('there was an error 2');return;}
+
+/* works :  $.get('/galleries/'+$( "#sortable" ).attr( "gid" )+'/sort/id?', '_method=put&authenticity_token='+AUTH_TOKEN+'&'+$(this).sortable('serialize')); */
+
+        var postdata = { 
+        'authenticity_token':AUTH_TOKEN          
+        }   
+         
+       $.ajax({
+          url: '/galleries/'+$( "#sortable" ).attr( "gid" )+'/sort/id?'+$(this).sortable('serialize'),
+          data: postdata,
+          type: 'post',  
+          beforeSend: function (req){
+            slideshow.loader(false);
+            console.log('sorting start')
+          },
+          error: function (textStatus, errorThrown){
+            slideshow.loader(true);
+            console.log('sorting error')
+          },
+          complete: function (req){
+            // call global loader func
+            slideshow.loader(true);
+            console.log('sorting complete')
+          },
+       }
+      ); // end call
+    } // end update
     });
+
     $( "#sortable" ).disableSelection();
   });
 
-/*       dropOnEmpty: false,
-       cursor: 'crosshair',
-       items: 'li',
-       opacity: 0.4,
-       scroll: true,
-
-       update: function() {
-    $ .post('/galleries/sort', 
-        '_method=put&authenticity_token='+AUTH_TOKEN+'&'+$(this).sortable('serialize'));
-    });
-
-       */
+ 
