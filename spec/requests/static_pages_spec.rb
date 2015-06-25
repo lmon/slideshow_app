6611b338983b546302d38d44ffb9ee91bb67a362
lmon <luke@lucasmonaco.com>
2015-06-25 10:11:43 -0400

    updating tests to confirm to latest copy changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,19 +6,19 @@ let(:base_title) { 'Slideshow App | ' }
 
  subject { page }
 
-  shared_examples_for "all static pages" do
+  shared_examples_for "all_static_pages" do
     it { should have_selector('h1', text: heading) }
     it { should have_title(page_title) }
   end
 
   describe "Home page" do
     before { visit root_path }
-    it { should have_content('App Home') }
+    it { should have_content('Slideshow App') }
 
-    let(:heading)    { 'My Slideshow App' }
+    let(:heading)    { 'Slideshow App' }
     let(:page_title) { "#{base_title} Home" }
 
-    it_should_behave_like "all static pages"
+    it_should_behave_like "all_static_pages"
 
     #it { should have_title("#{base_title} Home") }
     #it { should have_content('My Slideshow App') }
@@ -28,7 +28,7 @@ let(:base_title) { 'Slideshow App | ' }
     before { visit help_path }
     let(:heading)    { 'Help' }
     let(:page_title) { "#{base_title} Help" }
-    it_should_behave_like "all static pages"
+    it_should_behave_like "all_static_pages"
 
     it { should have_content('Help Page') }
   end
